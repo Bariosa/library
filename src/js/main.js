a6bf60811be65cbb2efdb943a8c88ba38f31c8ec
Anastasiia <bardieranastasi@gmail.com>
2023-11-21 16:22:25 +0200

    Створили файли стилів для проєкту

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+import $ from "./lib/lib";
+
+$("#first").on("click", () => {
+  $("div").eq(1).fadeToggle(800);
+});
+
+$("[data-count='second']").on("click", () => {
+  $("div").eq(2).fadeToggle(800);
+});
+
+$("button")
+  .eq(2)
+  .on("click", () => {
+    $(".w-500").fadeToggle(800);
+  });

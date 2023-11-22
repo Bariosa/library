7a2e26f2bcf402ff40c116155659f9fb7c2213c7
Anastasiia <bardieranastasi@gmail.com>
2023-11-22 15:20:33 +0200

    Створили modals

--- src/js/main.js
+++ src/js/main.js
@@ -14,15 +14,33 @@ $("button")
     $(".w-500").fadeToggle(800);
   });
 
-$(".wrap").html(
-  `  <div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" id="dropDownMenuButton">Dropdown button</button>
-    <div class="dropdown-menu" data-toggle-id="dropDownMenuButton">
-      <a href="#" class="dropdown-item">Action</a>
-      <a href="#" class="dropdown-item">Action#2</a>
-      <a href="#" class="dropdown-item">Action#3</a>
-    </div>
-  </div>`,
+$("#trigger").click(() =>
+  $("#trigger").createModal({
+    text: {
+      title: "Modal title",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quamcorporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus",
+    },
+    btns: {
+      count: 3,
+      settings: [
+        ["Close", ["btn-danger", "mr-10"], true],
+        [
+          "Save changes",
+          ["btn-success"],
+          false,
+          () => {
+            alert("Данные сохранены");
+          },
+        ],
+        [
+          "Another btn",
+          ["btn-warning", "ml-10"],
+          false,
+          () => {
+            alert("Hello world");
+          },
+        ],
+      ],
+    },
+  }),
 );
-
-$(".dropdown-toggle").dropdown();
